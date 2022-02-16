ddd8087b82961714664cbe6f76ab7f294e9e4a84
Code_Repo <huynhloc0411@hotmail.com>
2022-02-16 10:16:54 +0700

    UncommentCode

--- KMeans_Applications.py
+++ KMeans_Applications.py
@@ -5,27 +5,27 @@ from KMeans import KMeansClustering
 
 #   1. MNIST Classification
 #   2. Object Segmentation
-# def plot_seg(data, labels, values, origin_img):
-#     data[labels == 0] = values[0]
-#     data[labels == 1] = values[1]
-#     data[labels == 2] = values[2]
+def plot_seg(data, labels, values, origin_img):
+    data[labels == 0] = values[0]
+    data[labels == 1] = values[1]
+    data[labels == 2] = values[2]
 
-#     img = data.reshape(origin_img.shape)
-#     plt.imshow(img)
-#     plt.axis('off')
-#     plt.show()
+    img = data.reshape(origin_img.shape)
+    plt.imshow(img)
+    plt.axis('off')
+    plt.show()
 
 
-# if __name__ == "__main__":
-#     img_path = 'O:\My_Documents\MACHINE LEARNING\Datasets\girl.jpg'
-#     img = mpimg.imread(img_path)
-#     img_height, img_width, channels = img.shape
-#     data = img.reshape(-1, 3)
-#     no_classes = 3
+if __name__ == "__main__":
+    img_path = 'O:\My_Documents\MACHINE LEARNING\Datasets\girl.jpg'
+    img = mpimg.imread(img_path)
+    img_height, img_width, channels = img.shape
+    data = img.reshape(-1, 3)
+    no_classes = 3
 
-#     model = KMeansClustering(data, no_classes)
-#     values, labels = model.fit()
-#     plot_seg(data, labels, values, img)
+    model = KMeansClustering(data, no_classes)
+    values, labels = model.fit()
+    plot_seg(data, labels, values, img)
 
 
 #   3. Image Compression
@@ -53,14 +53,3 @@ if __name__ == "__main__":
     model = KMeansClustering(data, no_classes)
     values, labels = model.fit()
     plot_seg(data, labels, values, img)
-
-
-
-
-
-
-
-
-
-
-
